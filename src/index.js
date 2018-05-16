c8f7e20e4ef78439fcf18bb87d3f92e006d38947
Nikita Skorodumov <mr-zerx@yandex.ru>
2018-05-17 00:39:37 +0500

    Сделал 6 заданий

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
+    target.addEventListener(eventName, fn );
 }
 
 /*
@@ -20,6 +21,8 @@ function addListener(eventName, target, fn) {
    removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
  */
 function removeListener(eventName, target, fn) {
+    target.removeEventListener(eventName, fn );
+  
 }
 
 /*
@@ -31,6 +34,7 @@ function removeListener(eventName, target, fn) {
    skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
  */
 function skipDefault(eventName, target) {
+    target.addEventListener(eventName, ()=> event.preventDefault() );
 }
 
 /*
@@ -42,6 +46,9 @@ function skipDefault(eventName, target) {
    emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
  */
 function emulateClick(target) {
+    var event = new Event('click');
+    
+    target.dispatchEvent(event);
 }
 
 /*
@@ -54,6 +61,11 @@ function emulateClick(target) {
    delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
  */
 function delegate(target, fn) {
+    target.addEventListener('click', (e) => {
+        if (e.target.tagName === 'BUTTON') {
+            fn();
+        }
+    })
 }
 
 /*
@@ -66,6 +78,24 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
+    target.addEventListener('click', func );
+        
+    function func() {
+        fn();
+        target.removeEventListener('click', func );
+    }
+
+    // Можно сделать через промис : 
+    // target.addEventListener('click', prom);
+  
+    // function prom() {
+    //     const promise = new Promise((resolve) => {
+    //         fn();
+    //         resolve();
+    //     })
+        
+    //     promise.then (target.removeEventListener('click', prom));
+    // }
 }
 
 export {
